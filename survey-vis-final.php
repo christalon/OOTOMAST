4dd2fa0840843945110984fc81d408b61654ed3b
kielarano <33443977+kielarano@users.noreply.github.com>
2019-11-02 17:58:56 +0800

    Survey Results Visualization
    Started on results visualization

---
+++ survey-vis-final.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>OOTOMAST</title>
+        
+        <link href="http://localhost/OOTOMAST/css/c3.css" rel="stylesheet">   
+
+        <script src="http://localhost/OOTOMAST/js/d3.min.js"></script>
+        <script src="http://localhost/OOTOMAST/js/c3.min.js"></script>
+
+        <style>
+            table {
+                border-collapse: collapse;
+                border: 2px black solid;
+                font: 12px sans-serif;
+                margin-left:auto; 
+                margin-right:auto;
+            }
+
+            td {
+                border: 1px black solid;
+                padding: 5px;
+            }
+        </style>
+    </head>
+
+    <body>
+        <header>
+        <div id="Title" style="margin: 15px 10px ; display: flex;">
+            <h1 style="font-size: 25px"> Results </h1>
+        </div>
+        </header>
+
+        <div id="Visualization" style="margin: 15px 10px">
+        <script>
+            
+        </script>
+        </div>
+    </body>
