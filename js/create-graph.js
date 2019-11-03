cbd02caade4d1e60fb5d44dca016061a2abb199c
kielarano <33443977+kielarano@users.noreply.github.com>
2019-11-04 01:47:37 +0800

    Tried starting with reading csv files with Papaparse
    keyword "tried" :(

--- js/create-graph.js
+++ js/create-graph.js
@@ -2,7 +2,15 @@
  * Parse the data and create a graph with the data.
  */
 function parseData(createGraph) {
-
+	Papa.parse("http://localhost/OOTOMAST/csv/3-sample_results.csv", {
+    	download: true,
+    	step: function(row) {
+        	console.log("Row:", row.data);
+    	},
+    	complete: function() {
+        	console.log("All done!");
+    	}
+	});
 }
 
 function createGraph(data)	{
