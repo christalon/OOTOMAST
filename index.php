7c056c1dfdad3aec76f581742cd10d74650fc673
Christian Talon <criscore98@gmail.com>
2020-04-02 10:25:23 +0800

    Added border-radius
    * Added border radius to add survey form

--- index.php
+++ index.php
@@ -270,7 +270,7 @@
 
 		<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
 			<div class="modal-dialog modal-dialog-centered" role="document">
-				<div class="modal-content">
+				<div class="modal-content" style="border-radius: 20px;">
 				<div class="modal-header">
 					<h5 class="modal-title" id="exampleModalLongTitle">Add Survey</h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
